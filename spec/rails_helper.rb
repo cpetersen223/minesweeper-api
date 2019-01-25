e5f1b5e0a0da26edbc79c69bb473f30596ff8cf0
Christian Petersen <christianpetersen.ve@gmail.com>
2019-01-25 20:12:03 -0300

    Config rubocop defaults and fix the errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,14 +1,14 @@
-require 'simplecov'
+require "simplecov"
 SimpleCov.start
 
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require "spec_helper"
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
 # Prevent database truncation if the environment is production
-abort('The Rails environment is running in production mode!') if Rails.env.production?
-require 'rspec/rails'
-require 'database_cleaner'
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require "rspec/rails"
+require "database_cleaner"
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
@@ -31,7 +31,7 @@ end
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+# Dir[Rails.root.join("spec", "support", "**", "*.rb")].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
